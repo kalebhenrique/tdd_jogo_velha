f3a57c63132926e9a52704ac48bcdcdaec11f6e9
kalebhenrique <kalebhenriquebr@gmail.com>
2023-09-23 19:31:47 -0300

    test case 2 passou

--- velha.cpp
+++ velha.cpp
@@ -34,6 +34,8 @@ int VerificaVelha(int velha[3][3]) {
     for (int i = 1; i <= 2; i++) {
         if (velha[0][i] == 1 && velha[1][i] == 1 && velha[2][i] == 1) {
             return 1;
+        } else if (velha[i][0] == 1 && velha[i][1] == 1 && velha[i][2] == 1) {
+            return 1;
         }
     }
 
