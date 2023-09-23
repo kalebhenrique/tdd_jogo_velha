44629129e1b0cc48fb7396e1a3656ae648eba006
kalebhenrique <kalebhenriquebr@gmail.com>
2023-09-23 15:04:58 -0300

    formatando p/ cpplint

--- velha.cpp
+++ velha.cpp
@@ -14,26 +14,26 @@
  */
 
 int VerificaVelha(int velha[3][3]) {
-	int quantidadeX = 0;
-	int quantidadeO = 0;
+    int quantidadeX = 0;
+    int quantidadeO = 0;
 
-	for (int linha = 0; linha <= 2; linha++) {
-		for (int coluna = 0; coluna <= 2; coluna++) {
-			if (velha[linha][coluna] == 1) { 
-			quantidadeX++;
-			} else if (velha[linha][coluna] == 2) {
-				quantidadeO++;
-			}
-		}
-	}
+    for (int linha = 0; linha <= 2; linha++) {
+        for (int coluna = 0; coluna <= 2; coluna++) {
+            if (velha[linha][coluna] == 1) {
+                quantidadeX++;
+            } else if (velha[linha][coluna] == 2) {
+                quantidadeO++;
+            }
+        }
+    }
 
-	if (verificaDesequilibrioXO(quantidadeX, quantidadeO)) {
-		return -2;
-	}
+    if (verificaDesequilibrioXO(quantidadeX, quantidadeO)) {
+        return -2;
+    }
 
-	return 0;
+    return 0;
 }
 
 bool verificaDesequilibrioXO(int X, int O) {
-	return X > (O + 1) || (X + 1) < O;
+    return X > (O + 1) || (X + 1) < O;
 }
