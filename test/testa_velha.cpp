15116173638aa1175308d2bf21377d5665a294dd
kalebhenrique <kalebhenriquebr@gmail.com>
2023-09-23 15:04:46 -0300

    test case 1 passou

--- test/testa_velha.cpp
+++ test/testa_velha.cpp
@@ -8,12 +8,17 @@
 #define CATCH_CONFIG_MAIN
 #include "../catch.hpp"
   
-TEST_CASE( "Jogo inválido: X tem dois ou mais que O", "[single-file]" ) {
-	int teste1[3][3]= {   { 2, 0, 1 }, 
-	                      { 0, 0, 1 },
-						  { 0, 0, 1 }
+TEST_CASE( "Jogo inválido: há desiquilibrio entre X e O", "[single-file]" ) {
+	int vitoriaInjusta[3][3] = {    { 1, 0, 2 }, 
+									{ 0, 0, 2 },
+									{ 0, 0, 2 }
+							   };
+	int tudoX[3][3] = {   { 1, 1, 1 }, 
+	                      { 1, 1, 1 },
+						  { 1, 1, 1 }
 					  };
 					  
-    REQUIRE( VerificaVelha(teste1) == -2 );
+    REQUIRE( VerificaVelha(vitoriaInjusta) == -2 );
+	REQUIRE( VerificaVelha(tudoX) == -2 );
 }
  
